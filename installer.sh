770f930028567496d019e4d1c91aa5ea7648f017
Angelo Passaro <14933081+angelopassaro@users.noreply.github.com>
2024-05-13 21:26:38 +0200

    fix

--- installer.sh
+++ installer.sh
@@ -9,21 +9,30 @@ go install github.com/tomnomnom/assetfinder@latestsudo mv ~/go/bin/assetfinder /
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latestsudo mv ~/go/bin/subfinder /usr/local/bin/
 go install github.com/sensepost/gowitness@latestsudo mv ~/go/bin/gowitness /usr/local/bin/
 
-curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zipunzip findomain-linux.zip
-chmod +x findomainsudo mv findomain /usr/bin/local/findomain
-rm -rf findomainrm findomain-linux.zip
+curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
+unzip findomain-linux.zip
+chmod +x findomain
+sudo mv findomain /usr/bin/local/findomain
+rm -rf findomain
+rm findomain-linux.zip
 
-git clone https://github.com/0xKayala/ParamSpider /opt/ParamSpidercd /opt/ParamSpider
-sudo python3 -m pip install -r requirements.txtln -s /opt/ParamSpider/paramspider.py /usr/local/bin/paramspider
+git clone https://github.com/0xKayala/ParamSpider /opt/ParamSpider
+cd /opt/ParamSpider
+sudo python3 -m pip install -r requirements.txt
+ln -s /opt/ParamSpider/paramspider.py /usr/local/bin/paramspider
 cd -
 sudo git clone https://github.com/maurosoria/dirsearch.git --depth 1 /opt/dirsearch
-cd /opt/dirsearchsudo python3 -m pip install -r requirements.txt
-ln -s /opt/ParamSpider/dirsearch.py /usr/local/bin/dirsearchcd -
+cd /opt/dirsearch
+sudo python3 -m pip install -r requirements.txt
+ln -s /opt/ParamSpider/dirsearch.py /usr/local/bin/dirsearch
+cd -
 
-git clone https://github.com/m4ll0k/SecretFinder.git secretfindercd secretfinder
+git clone https://github.com/m4ll0k/SecretFinder.git secretfinder
+cd secretfinder
 python3 -m pip install -r requirements.txt
 sudo mv SecretFinder.py /usr/local/bin/secretfinder
 cd -rm -rf secretfinder
 wget https://raw.githubusercontent.com/w9w/JSA/main/templates/credentials-disclosure-all.yaml
 wget https://raw.githubusercontent.com/w9w/JSA/main/templates/some-PIIs.yamlmkdir ~/nuclei-templates/JSA
-mv some-PIIs.yaml ~/nuclei-templates/JSA/mv credentials-disclosure-all.yaml ~/nuclei-templates/JSA/
+mv some-PIIs.yaml ~/nuclei-templates/JSA/
+mv credentials-disclosure-all.yaml ~/nuclei-templates/JSA/
