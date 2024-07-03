a7ced15e1877d763169a278e98d1c7d785141b1d
Angelo Passaro <14933081+angelopassaro@users.noreply.github.com>
2024-07-03 17:18:53 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -21,8 +21,7 @@ sudo mv ~/go/bin/gowitness /usr/local/bin/
 curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
 unzip findomain-linux.zip
 chmod +x findomain
-sudo mv findomain /usr/bin/local/findomain
-rm -rf findomain
+sudo mv findomain /usr/local/bin/findomain
 rm findomain-linux.zip
 
 
