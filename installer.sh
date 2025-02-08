bc6fcf31817c9cd3eb55a704444a7cd10407c822
angelopassaro <angelopassaro1@gmail.com>
2025-02-08 06:23:44 -0500

    Added install with pipx

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ sudo mv ~/go/bin/httpx /usr/local/bin/
 go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
 sudo mv ~/go/bin/dnsx /usr/local/bin/
 go install github.com/tomnomnom/assetfinder@latest
-sudo mv ~/go/bin/assetfinder /usr/local/bin/
+udo mv ~/go/bin/assetfinder /usr/local/bin/
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 sudo mv ~/go/bin/subfinder /usr/local/bin/
 go install -v github.com/projectdiscovery/interactsh/cmd/interactsh-client@latest
@@ -29,33 +29,14 @@ chmod +x findomain
 sudo mv findomain /usr/local/bin/findomain
 sudo rm findomain-linux.zip
 
+pipx ensurepath 
 
-sudo git clone https://github.com/devanshbatham/paramspider /opt/paramspider
-cd /opt/paramspider
-sudo python3 -m pip install .
-sudo rm -rf /opt/paramspider
-cd -
-
-
-sudo git clone https://github.com/maurosoria/dirsearch.git --depth 1 /opt/dirsearch
-cd /opt/dirsearch
-sudo python3 -m pip install -r requirements.txt
-sudo ln -s /opt/dirsearch/dirsearch.py /usr/local/bin/dirsearch
-cd -
-
-sudo git clone https://github.com/GerbenJavado/LinkFinder.git /opt/LinkFinder
-cd /opt/LinkFinder
-python3 -m pip install -r requirements.txt
-cd -
-sudo rm /opt/LinkFinder
+pipx install git+https://github.com/devanshbatham/paramspider 
+pipx install git+https://github.com/maurosoria/dirsearch.git 
+pipx install git+https://github.com/GerbenJavado/LinkFinder.git --include-deps   
+pipx install git+https://github.com/angelopassaro/SecretFinder
 
 
-sudo git clone https://github.com/m4ll0k/SecretFinder.git secretfinder
-cd secretfinder
-python3 -m pip install -r requirements.txt
-sudo mv SecretFinder.py /usr/local/bin/secretfinder
-cd -
-sudo rm -rf secretfinder
 
 wget https://raw.githubusercontent.com/w9w/JSA/main/templates/credentials-disclosure-all.yaml
 wget https://raw.githubusercontent.com/w9w/JSA/main/templates/some-PIIs.yaml
