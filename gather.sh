14b7f5477929241e60401165660f85525925ae12
angelopassaro <angelopassaro1@gmail.com>
2025-02-12 09:45:47 -0500

    Refactor outputs for passive scan

--- gather.sh
+++ gather.sh
@@ -34,33 +34,36 @@ echo -e "${CYAN}                                                                
 #intercatsh-client_path
 
 
-dns_result=$(pwd)/dns_ptr.txt  # domain retrived from IP
-katana_result=$(pwd)/katana_result.txt # katana static finding
-targets=$(pwd)/target.txt # working file
-live_target=$(pwd)/live_target.txt
-domains_tmp=$(pwd)/domains.tmp # working file for domain
-subdomains=$(pwd)/subdomains.txt # list of valif subdomain
-nuclei_vuln=$(pwd)/nuclei_vuln.txt
-technologies=$(pwd)/technologies.txt
-cves=$(pwd)/cves.txt
-targets_url=$(pwd)/targets_url.txt # urls with param
-dalfox_out=$(pwd)/dalfox.txt
-dalfox_blind_out=$(pwd)/dalfox_blind.txt
-statics=$(pwd)/statics.txt
-findings=$(pwd)/findings
-nuclei_findings=$(pwd)/nuclei_findings.txt
-nuclei_headers=$(pwd)/nuclei_missing_headers.txt
-dirsearch=$(pwd)/dirsearch.txt
-takeover=$(pwd)/takeover.txt
-log=$(pwd)/log.log
-dalfox_log=$(pwd)/dalfox.log
-link=$(pwd)/link.txt
-mapping=$(pwd)/mapping.txt
-domains=$(pwd)/domains.txt
-interact_session=$(pwd)/interact_session.txt
-interact_output=$(pwd)/interact_output.txt
-response=$(pwd)/responses
-js=$(pwd)/js
+katana_result="katana_result.txt" # katana static finding
+targets="" # working file
+live_target=""
+domains_tmp="" # working file for domain
+subdomains="" # list of valif subdomain
+nuclei_vuln=""
+technologies=""
+cves=""
+targets_url="" # urls with param
+dalfox_out=""
+dalfox_blind_out=""
+statics=""
+findings=""
+nuclei_findings=""
+nuclei_headers=""
+dirsearch=""
+takeover=""
+dalfox_log=""
+link=""
+mapping=""
+domains=""
+interact_session=""
+interact_output=""
+response=""
+js=""
+dir_name=""
+log=""
+dns_result=""
+
+
 
 s_flag=false # flag for search subdomains
 m_flag=false # flag for use mapper
@@ -76,6 +79,41 @@ exit_abnormal() {
   exit 1
 }
 
+update_variable() {
+    #katana_result=$dir_name/katana_result.txt # katana static finding  TODO REMOVE
+    mkdir "$dir_name/scope/"
+    mkdir "$dir_name/scans/"
+    mkdir "$dir_name/nmap/"
+
+
+    targets=$dir_name/scope/target.txt # working file
+    live_target=$dir_name/scope/live_target.txt
+    domains_tmp=$dir_name/domains.tmp # working file for domain
+    subdomains=$dir_name/scope/subdomains.txt # list of valif subdomain
+    nuclei_vuln=$dir_name/nuclei_vuln.txt
+    technologies=$dir_name/technologies.txt
+    cves=$dir_name/cves.txt
+    targets_url=$dir_name/targets_url.txt # urls with param
+    dalfox_out=$dir_name/dalfox.txt
+    dalfox_blind_out=$dir_name/dalfox_blind.txt
+    statics=statics.txt
+    findings=findings.txt
+    nuclei_findings=nuclei_findings.txt
+    nuclei_headers=$dir_name/nuclei_missing_headers.txt
+    dirsearch=$dir_name/dirsearch.txt
+    takeover=$dir_name/takeover.txt
+    dalfox_log=$dir_name/dalfox.log
+    link=link.txt
+    mapping=$dir_name/mapping.txt
+    domains=$dir_name/domains.txt
+    interact_session=$dir_name/interact_session.txt
+    interact_output=$dir_name/interact_output.txt
+    response=$dir_name
+    js=$dir_name/js
+    log=$dir_name/log.log
+    dns_result=$dir_name/scope/dns_ptr.txt  # domain retrived from IP
+    scans=$dir_name/scans
+}
 
 check_input_type() {
     local input=$1
@@ -129,60 +167,60 @@ check_ip_in_cidr() {
 
 
 check_scope() {
-    local result_tmp=$(pwd)/result.tmp 
-    local temp=$(pwd)/temp.tmp
+     local result_tmp=$dir_name/result.tmp 
+     local temp=$dir_name/temp.tmp
     
-    echo -e "${YELLOW}[-] Checking the scope${NC}"
-    if [ -n "$ip" ]; then
-        sort -u $1 | dnsx -silent -a -resp -nc > $result_tmp
+     echo -e "${YELLOW}[-] Checking the scope${NC}"
+     if [ -n "$ip" ]; then
+         sort -u $1 | dnsx -silent -a -resp -nc > $result_tmp
 
-        input_type=$(check_input_type "$ip")
-        case $input_type in
-            "CIDR")
-                while IFS= read -r line; do
-                    ip_wk=$(echo -e "$line" | awk '{print $3}' | grep -oP '(?<=\[).*?(?=\])')
-                    if check_ip_in_cidr "$ip_wk" "$ip"; then
-                        echo $line | awk '{print $1}' >> $targets 
-                    fi
-                done < "$result_tmp"
-                ;;
-            "IP")
-                while IFS= read -r line; do
-                    ip_wk=$(echo -e "$line" | awk '{print $3}' | grep -oP '(?<=\[).*?(?=\])')
-                    if check_ip_equality "$ip_wk" "$ip"; then
-                        echo $line | awk '{print $1}' >> $targets 
-                    fi
-                done < "$result_tmp"
-                ;;
-            *)
-                echo -e "${RED}Wrong IP: $ip_tmp${NC}"
-                ;;
-        esac
-        rm $result_tmp
-    else
-        local valid_domains=()
-        while IFS= read -r line; do
-            valid_domains+=("$line")
-        done <  "$(pwd)/$domain"
+         input_type=$(check_input_type "$ip")
+         case $input_type in
+             "CIDR")
+                 while IFS= read -r line; do
+                     ip_wk=$(echo -e "$line" | awk '{print $3}' | grep -oP '(?<=\[).*?(?=\])')
+                     if check_ip_in_cidr "$ip_wk" "$ip"; then
+                         echo $line | awk '{print $1}' >> $targets 
+                     fi
+                 done < "$result_tmp"
+                 ;;
+             "IP")
+                 while IFS= read -r line; do
+                     ip_wk=$(echo -e "$line" | awk '{print $3}' | grep -oP '(?<=\[).*?(?=\])')
+                     if check_ip_equality "$ip_wk" "$ip"; then
+                         echo $line | awk '{print $1}' >> $targets 
+                     fi
+                 done < "$result_tmp"
+                 ;;
+             *)
+                 echo -e "${RED}Wrong IP: $ip_tmp${NC}"
+                 ;;
+         esac
+         rm $result_tmp
+     else
+         local valid_domains=()
+         while IFS= read -r line; do
+             valid_domains+=("$line")
+         done <  "$dir_name/$domain"
 
 
-        while IFS= read -r domain_value; do
-            found=false
-            for valid_domain in "${valid_domains[@]}"; do
-                if [[ "$domain_value" == *"$valid_domain" || "$domain_value" == "$valid_domain" || "$domain_value" == *".$valid_domain" ]]; then
-                    found=true
-                    break
-                fi
-            done
+         while IFS= read -r domain_value; do
+             found=false
+             for valid_domain in "${valid_domains[@]}"; do
+                 if [[ "$domain_value" == *"$valid_domain" || "$domain_value" == "$valid_domain" || "$domain_value" == *".$valid_domain" ]]; then
+                     found=true
+                     break
+                 fi
+             done
 
-            if [ "$found" = true ]; then
-                echo "$domain_value" >> $targets
-            fi
-        done < "$1"
-    fi
+             if [ "$found" = true ]; then
+                 echo "$domain_value" >> $targets
+             fi
+         done < "$1"
+     fi
 
     if [[ -s $targets ]]; then
-        sort -u $targets > $temp && mv $temp $targets
+        sort -u "$targets" > "$temp" &&  mv "$temp" "$targets"
     fi
 
     httpx -l $targets --silent  -sr -srd $response > $live_target 
@@ -192,8 +230,7 @@ check_scope() {
 
 
 nmap_check(){
-    mkdir $(pwd)/nmap/
-    local nmap_result=$(pwd)/nmap/all
+    local nmap_result=$dir_name/nmap/all
     echo -e "${YELLOW}[-] Start NMAP enumeration${NC}"
     nmap -sC -sV $ip  -oA $nmap_result | grep -o 'DNS:[^,]*' | awk -F: '{print $2}'  | sort | uniq > $dns_result
     echo -e "${GREEN}[+] NMAP enumeration completed. Result saved in:${NC} ${CYAN}$nmap_result${NC}"
@@ -213,38 +250,36 @@ dns_enum() {
 
 statics_enum() {
     echo -e "${YELLOW}[-] Start statics enumeration with Katana${NC}"
-    katana -silent -list $live_target  -d 5 -jc -kf all -fx -xhr -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg > $katana_result 2>> $log
-    urlfinder -silent -dL $live_target >> $katana_result 2>> $log
-    echo -e "${GREEN}[+] Statics enumeration completed. Result saved in:${NC} ${CYAN} $katana_result${NC}"
-    #echo -e "${YELLOW}[-] Recovering domains${NC}"
-    #for url in $(cat $katana_result); do
-	#    echo -e "$url" | grep -oP "(?<=://)([^/]+)" >> "$domains_tmp"
-    #done
-    
-    #check_scope $domains_tmp
-    #rm $domains_tmp
-    #echo -e "${GREEN}[+] Domains recovered${NC}"
+
+
+    for url in $(cat $live_target); do
+        local result_katana="$dir_name/scans/${url#*//}/$katana_result" 
+        mkdir "$dir_name/scans/${url#*//}"
+        katana -silent -u $url  -d 5 -jc -kf all -fx -xhr -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg > $result_katana 2>> $log
+        urlfinder -silent -d $url >> $result_katana 2>> $log
+    done
+    echo -e "${GREEN}[+] Statics enumeration completed. Result saved in:${NC} ${CYAN} $scans ${NC}"
 }
 
 
 search_subdomain() {
-    local sub1="$(pwd)/subdomains-1.txt"
-    local sub2="$(pwd)/subdomains-2.txt" 
-    local sub3="$(pwd)/subdomains-3.txt" 
-    local tmp="$(pwd)/tmp.txt" 
+    local sub1="$dir_name/subdomains-1.txt"
+    local sub2="$dir_name/subdomains-2.txt" 
+    local sub3="$dir_name/subdomains-3.txt" 
+    local tmp="$dir_name/tmp.txt" 
     
     echo -e "${YELLOW}[-] Start finding subdomains${NC}"
     cat $dns_result >> $subdomains # need to check the IP for the resolved DNS
     cat $dns_result | assetfinder --subs-only | grep -v "[INF]"  > $sub1 2>> $log &
     findomain -q -f $dns_result > $sub2 2>> $log &
     subfinder -dL $dns_result -silent -all -nc > $sub3 2>> $log &
-
     wait
+
     echo -e "${YELLOW}[-] Merge of subdomains${NC}"
     cat $sub1 $sub2 $sub3 |alterx --silent -en > $tmp
 
     sort -u $tmp > $subdomains
-    rm $tmp $(pwd)/subdomains-*
+    rm $tmp $dir_name/subdomains-*
     check_scope $subdomains
     echo -e "${GREEN}[+] Subdomain enumeration completed. Result saved in:${NC} ${CYAN}$subdomains${NC}"
     echo -e "${GREEN}[+] Valid targets saved in:${NC} ${CYAN}$targets${NC}\n${GREEN}[+] Live targets saved in:${NC}${CYAN}$live_target${NC}"
@@ -302,8 +337,52 @@ dalfox_check(){
 }
 
 secret_check(){
-    local temp=$(pwd)/temp.tmp
-    echo -e "${YELLOW}[-] Start secrets finding${NC}"
+    echo -e "${YELLOW}[-] Start secrets finding for live targets${NC}"
+    for dir in $(ls $scans);do
+        echo -e "${YELLOW}[-] Start secrets finding for: ${NC}${CYAN}$dir${NC}"
+        local temp=$scans/$dir/temp.tmp
+        katana  -u $scans/$dir/$katana_result --silent -em js -d 5 -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg > $temp 
+        sort -u $temp > $scans/$dir/$statics
+        echo "" > $temp
+
+        for i in $(cat $scans/$dir/$katana_result);do
+            python "$(pipx runpip LinkFinder show LinkFinder | awk -F ': ' '/Location/ {print $2 "/linkfinder.py"}')" -i $i -d -o cli | grep -v "Running against" | grep -v "^$" | grep -v "Invalid input defined or SSL error for:"  >>  $temp
+        done
+
+        sort -u $temp | grep -ivf clear-list.txt > $scans/$dir/$link
+        echo "" > $temp
+
+        if [[ -s  $scans/$dir/$statics ]]; then
+            mkdir $scans/$dir/findings
+            c=1
+            for i in $(cat  $scans/$dir/$statics);do  
+                secretfinder -i $i -g 'jquery;bootstrap;api.google.com' -o cli > $scans/$dir/findings/$c.txt
+                ((c=c+1))
+            done
+
+            cat  $scans/$dir/findings/* >> $temp
+            sort -u $temp > $scans/$dir/findings/$findings
+                echo -e "${YELLOW}[+] Secret findings for ${CYAN}$dir${NC}${YELLOW} completed. Results saved in the directory ${NC}${CYAN}$scans/$dir/findings/${NC}${YELLOW}unique result saved in: ${NC}${CYAN}$scans/$dir/findings/$findings${NC}"
+            else
+                echo -e "${YELLOW}[-] Statics not found for ${CYAN}$dir${NC}.${YELLOW}SecretFinder skipped${NC}"
+            fi
+        rm $temp
+        echo -e "${YELLOW}[-] Start secrets finding with nuclei for ${CYAN}$dir${NC}"
+        nuclei -t javascript/enumeration -u $dir --silent > $scans/$dir/$nuclei_findings
+        #https://github.com/w9w/JSA/tree/main/templates
+        if [[ -n "$domain" ]];then
+            nuclei -t JSA -u $dir --silent | grep "PII" | grep -v "\"\""  >> $scans/$dir/$nuclei_findings
+        else	
+            nuclei -t JSA -u $dir --silent | grep "PII" | grep -v "\"\""  >> $scans/$dir/$nuclei_findings
+        fi
+        echo -e "${GREEN}[+] Secret findings completed for ${CYAN}$dir${NC}. Results saved in:${NC}${CYAN}$scans/$dir/$nuclei_findings${NC} ${GREEN}and${NC} ${CYAN}$scans/$dir/$link${NC}"
+
+    done
+        echo -e "${GREEN}[+] Secret findings for live targets${NC}${YELLOW}completed${NC}"
+
+    exit
+
+
     katana  -list $katana_result --silent -em js -d 5 -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg > $temp 
     sort -u $temp > $statics
     echo "" > $temp
@@ -360,11 +439,11 @@ screenshot() {
     #if [ -n "$domain" ]; then
     #    gowitness file -f $live_target --screenshot-fullpage -q 2>$log
     if [ -n "$ip" ]; then
-        gowitness scan nmap -f nmap/all.xml -o --service-contains http --screenshot-fullpage -q 2>>$log
+        gowitness scan nmap -f $dir_name/nmap/all.xml -o --service-contains http --screenshot-fullpage -s $dir_name/screenshot -q 2>>$log
     else
-        gowitness scan file -f $live_target --screenshot-fullpage -q 2>>$log
+        gowitness scan file -f $live_target --screenshot-fullpage $dir_name/screenshot -q 2>>$log
     fi
-    echo -e "${GREEN}[+] Screenshot taken. Results saved in:${NC}${CYAN}$(pwd)${NC}\n${YELLOW}Run ${CYAN}gowitness report server${NC}${YELLOW} for check the report${NC}"
+    echo -e "${GREEN}[+] Screenshot taken. Results saved in:${NC}${CYAN}$dir_name/screenshot${NC}\n${YELLOW}Run ${CYAN}gowitness report server${NC}${YELLOW} for check the report${NC}"
 }
 
 
@@ -382,15 +461,11 @@ passive() {
     date >> $log
     check_input_type $ip >/dev/null
     echo -e "${GREEN}[+] Working for the IP/CIDR:${NC} ${CYAN}$ip${NC}"
-    echo -e "${GREEN}[+] The output will be saved in the directory:${NC}${CYAN}$(pwd)${NC}"
+    echo -e "${GREEN}[+] The output will be saved in the directory:${NC}${CYAN} $dir_name${NC}"
     nmap_check
-    dns_enum
+    dns_enum    
     search_subdomain
     statics_enum
-    #if [ ! -s $target ]; then
-    #    echo -e "${GREEN}[+] DNS enumeration completed.${NC}${RED}0 Target. Quitting.${NC}"
-    #    exit 0
-    #fi
     screenshot
     secret_check
     echo -e "${GREEN}[+]Passive scans completed${NC}"
@@ -459,10 +534,14 @@ if [[ -z "$ip" && -z "$domain" ]]; then
   exit_abnormal
 else
   if [[ -n "$ip" ]]; then
+    dir_name="${ip%/*}"  
+    mkdir -p "$dir_name"
+    update_variable
     passive
   fi
 
   if [[ -n "$domain" ]]; then
+    update_variable
     domain
   fi
 
